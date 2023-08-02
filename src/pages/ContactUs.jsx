c68fdc1ad8a4374e308f5c2de8290ae3ed210780
sonke06 <sonkemlambo068@gmail.com>
2023-08-02 10:15:40 +0200

    changed button

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -78,7 +78,7 @@ const ContactUs = () => {
               <div class="form-group">
                 <div class="col-sm-offset-2 col-sm-10">
                   <button type="submit" class="btn btn-default">
-                    Submit
+                    Submit save
                   </button>
                 </div>
               </div>

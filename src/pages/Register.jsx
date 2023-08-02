3b628ecf10dc7a42327b9fdda1a0f6ae217cd365
Thozama3 <t.marasela@gmail.com>
2023-08-02 09:51:45 +0200

    span color

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,7 +14,7 @@ const Register = () => {
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
           <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
+           <span> We'll never share your email with anyone else.</span>
           </Form.Text>
         </Form.Group>
 
